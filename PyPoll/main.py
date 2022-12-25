3f5125f6d87cd24efdee2757c91f816225e42ba9
OlgaDlzk <olga.dolzhko@gmail.com>
2022-12-25 16:00:26 -0500

    finished total for PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,23 +2,40 @@ import os
 import csv
 
 print("Election Results")
-print("------------------------------")
-
-Total_Votes = 0
-
+print("---------------------")
 
 # load csv file
 csvpath = os.path.join('Resources', 'election_data.csv')
-outputfile = os.path.join('analysis', 'election_analysis.txt')
 
-#open csv file
+# set up variables
+
+# amount of total rows, not uncluding the header
+total_vote = 0
+
+# dict with key-value pair candidate: numer of votes
+candidates_list = {}
+
+# variables for the popular votes winner
+popular_votes = 0
+winner = ""
+
 with open(csvpath) as csvfile:
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile)
     csv_header = next(csvreader)
 
-    for row in csv_header:
-        Total_Votes = Total_Votes + 1
+    for row in csvreader:
+        # total amount of rows - header row = total amoun of votes 
+        total_vote +=1
+        # all the key values in dic are nunique. 
+        # So if candidate name is already added, it will skip that part and 
+        # just will add +1 to the vote count for this candidate
+        if row[2] not in candidates_list:
+            candidates_list[row[2]] = 1
+        else:
+            candidates_list[row[2]] += 1    
 
-    print("Total Votes: " + str(Total_Votes))    
+
+print("Total Votes: "+ str(total_vote))
+print(candidates_list)
+
